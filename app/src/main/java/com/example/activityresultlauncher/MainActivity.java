f9bdc832a9cfda16e1f441b64c26ca91e2a82b34
Micoder-dev <micoder.com@gmail.com>
2022-07-24 11:26:09 +0530

    Open the second activity for result - Send data

--- app/src/main/java/com/example/activityresultlauncher/MainActivity.java
+++ app/src/main/java/com/example/activityresultlauncher/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.activityresultlauncher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSendData = findViewById(R.id.btnSendData);
+        btnSendData.setOnClickListener(view -> {
+
+            openSecondActivityForResult();
+
+        });
+
     }
+
+    private void openSecondActivityForResult() {
+
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("number2", 2);
+
+    }
+
 }
