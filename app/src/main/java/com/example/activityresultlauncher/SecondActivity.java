2d65484462ea93f76ae6bd66f95e6d954b95f9fd
Micoder-dev <micoder.com@gmail.com>
2022-07-24 11:32:39 +0530

    Send back result data from the second activity to the main activity

--- app/src/main/java/com/example/activityresultlauncher/SecondActivity.java
+++ app/src/main/java/com/example/activityresultlauncher/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.activityresultlauncher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -13,6 +16,16 @@ public class SecondActivity extends AppCompatActivity {
 
         int number2 = getIntent().getIntExtra("number2", 0);
 
+        Button btnSendBackData = findViewById(R.id.btnSendDataBack);
+
+        btnSendBackData.setOnClickListener(view -> {
+
+            Intent intent = new Intent();
+            intent.putExtra("2x2", number2 * 2);
+            setResult(RESULT_OK, intent);
+            finish();
+
+        });
 
     }
 }
